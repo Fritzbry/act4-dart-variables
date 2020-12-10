210e40a3cd58b7af5c29e1cbf71ebed007d4490f
Fritzbry <71302344+Fritzbry@users.noreply.github.com>
2020-12-10 15:38:45 +0800

    Create variables.dart

---
+++ variables.dart
@@ -0,0 +1,8 @@
+void main(){
+    String firstName = "Jollibee";
+    String middleInitial = "E";
+    String lastName = "Lopez";
+    int age = 42;
+    double height = 1.83;
+   print("$firstName \n$middleInitial \n$lastName \n$age \n$height");
+}
